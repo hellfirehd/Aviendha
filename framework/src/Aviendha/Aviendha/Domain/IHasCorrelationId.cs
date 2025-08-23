148c2dd308870e7b08e3f36d48721af402cd10b2
Doug Wilson <d@dkw.io>
2025-08-23 16:29:09 -0700

    Moves IHasCorrelationId to Domain
    Moves the IHasCorrelationId interface from the Application Contracts layer to the Domain layer.
    
    This change reflects that the concept of a correlation ID is fundamentally a domain concern, rather than an application service concern.

--- framework/src/Aviendha/Aviendha/Domain/IHasCorrelationId.cs
+++ framework/src/Aviendha/Aviendha/Domain/IHasCorrelationId.cs
@@ -12,9 +12,7 @@
 // You should have received a copy of the GNU Affero General Public License along with this
 // program. If not, see <https://www.gnu.org/licenses/>.
 
-using Aviendha.Domain;
-
-namespace Aviendha.Application;
+namespace Aviendha.Domain;
 
 public interface IHasCorrelationId
 {
